ec2add43dfc9520e5d87fbfcaab3addd42a52226
5kylinee <konrad.mullerr@gmail.com>
2025-03-18 19:43:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 
 const audio = document.getElementById("bg-music");
 const playButton = document.getElementById("playButton");
+const volumeControl = document.getElementById("volumeControl");
+const volumeLabel = document.getElementById("volumeLabel");
 
 
 playButton.addEventListener("click", function () {
   if (audio.paused) {
     audio.play();
-    playButton.textContent = "Pause Music"; 
+    playButton.textContent = "Pause Music";
   } else {
     audio.pause();
-    playButton.textContent = "Play Music"; 
+    playButton.textContent = "Play Music";
   }
+});
+
+
+volumeControl.addEventListener("input", function () {
+  const volume = volumeControl.value / 100; 
+  audio.volume = volume;
+  volumeLabel.textContent = volumeControl.value + "%"; 
 });
