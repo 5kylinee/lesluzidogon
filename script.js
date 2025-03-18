841280d38ce6902ac74315f9a2090e392300b6c1
5kylinee <konrad.mullerr@gmail.com>
2025-03-18 19:41:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,13 @@
 const audio = document.getElementById("bg-music");
 const playButton = document.getElementById("playButton");
 
+
 playButton.addEventListener("click", function () {
   if (audio.paused) {
     audio.play();
-    playButton.textContent = "Pause Music";
+    playButton.textContent = "Pause Music"; 
   } else {
     audio.pause();
-    playButton.textContent = "Play Music";
+    playButton.textContent = "Play Music"; 
   }
 });
