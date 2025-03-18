4b1dec144c4f102d9a01b256fc6b39e84741711c
5kylinee <konrad.mullerr@gmail.com>
2025-03-18 22:12:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-<script>
-    document.addEventListener("DOMContentLoaded", function () {
-        const quoteText = document.querySelector(".quote-text");
 
-        const observer = new IntersectionObserver((entries) => {
-            entries.forEach(entry => {
-                if (entry.isIntersecting) {
-                    quoteText.classList.add("show");
-                }
-            });
-        }, {
-            threshold: 0.6 // Trigger when 60% of the section is visible
-        });
-
-        observer.observe(quoteText);
-    });
-</script>
